3f194d82ee7fc5b6864e4eac376b96d5f4a3a723
Sheharyar Naseer <hello@sheharyar.me>
2018-07-11 08:12:50 -0400

    Tests for Memento.Error.raise

---
+++ test/memento/error_test.exs
@@ -0,0 +1,22 @@
+defmodule Memento.Tests.Error do
+  use ExUnit.Case
+  require Memento.Error
+
+
+  describe "#raise" do
+    test "raises error with default message" do
+      assert_raise(Memento.Error, "Operation Failed", fn ->
+        Memento.Error.raise
+      end)
+    end
+
+
+    @message "Something Happened"
+    test "raises error with given message" do
+      assert_raise(Memento.Error, @message, fn ->
+        Memento.Error.raise(@message)
+      end)
+    end
+  end
+
+end
