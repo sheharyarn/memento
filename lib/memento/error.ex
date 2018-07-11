ebb0e28515373888372e07e224e8b86c3c64d20f
Sheharyar Naseer <hello@sheharyar.me>
2018-07-11 08:08:44 -0400

    Error.raise macro with defaults

--- lib/memento/error.ex
+++ lib/memento/error.ex
@@ -2,4 +2,14 @@ defmodule Memento.Error do
   defexception [:message]
 
   @moduledoc false
+  @default_message "Operation Failed"
+
+
+  # Raise a Memento.Error
+  defmacro raise(message \\ @default_message) do
+    quote do
+      raise Memento.Error, message: unquote(message)
+    end
+  end
+
 end
