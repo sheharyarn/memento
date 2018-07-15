fb1297205ebbd781a70a4291fb9885797b7612e4
Sheharyar Naseer <hello@sheharyar.me>
2018-07-15 03:33:44 -0400

    Memento.start and Memento.stop

--- lib/memento/memento.ex
+++ lib/memento/memento.ex
@@ -1,5 +1,43 @@
 defmodule Memento do
+  require Memento.Mnesia
+
+
   @moduledoc """
   Nothing here now, come back later
+
+  TODO: Convert this into an 'Application'
   """
+
+
+
+
+  # Public API
+  # ----------
+
+
+  @doc """
+  Start the Memento Application.
+
+  This starts Memento and `:mnesia` along with some sane application
+  defaults. See `:mnesia.start/0` for more details.
+  """
+  @spec start() :: Memento.Mnesia.result
+  def start do
+    Application.start(:mnesia)
+  end
+
+
+
+
+  @doc """
+  Stop the Memento Application.
+  """
+  @spec stop() :: Memento.Mnesia.result
+  def stop do
+    Memento.Mnesia.suppress_log fn ->
+      Application.stop(:mnesia)
+    end
+  end
+
+
 end
