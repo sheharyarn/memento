7679fbc53529ca8b18127a6d089b797318c3566b
Sheharyar Naseer <hello@sheharyar.me>
2018-07-17 12:28:43 -0400

    Printing Mnesia descriptions of the error as well

--- lib/memento/error/mnesia_exception.ex
+++ lib/memento/error/mnesia_exception.ex
@@ -1,28 +1,54 @@
 defmodule Memento.MnesiaException do
   alias __MODULE__
 
-  defexception [:message, :data]
-
   @moduledoc false
-  @message "Mnesia Operation failed with:\n     "
+  @newline "\n   "
+
+  defexception [:message, :data]
 
 
 
   # Re-raise Mnesia exits as Exceptions
   defmacro raise(data) do
-    quote do
-      data = MnesiaException.fetch(unquote(data))
-
-      raise MnesiaException,
-        message: (unquote(@message) <> inspect(data)),
-        data: data
+    quote(bind_quoted: [data: data]) do
+      raise MnesiaException.build(data)
     end
   end
 
 
+
+  # Build the Exception struct
+  def build(error) do
+    error = fetch(error)
+    message =
+      "Mnesia operation failed" <> @newline <>
+      info(error) <> @newline <>
+      "Mnesia Error: " <> inspect(error)
+
+    %MnesiaException{
+      data: error,
+      message: message,
+    }
+  end
+
+
+
   # Parse the data returned from an Mnesia Exit Exception
-  def fetch({:aborted, data}), do: data
-  def fetch({:error, data}),   do: data
-  def fetch(data),             do: data
+  defp fetch({:aborted, data}), do: data
+  defp fetch({:error, data}),   do: data
+  defp fetch(data),             do: data
+
+
+  # Fetch Mnesia's description of the error
+  defp info({code, _, _}), do: info(code)
+  defp info({code, _}),    do: info(code)
+  defp info(code) do
+    desc = :mnesia.error_description(code)
+
+    cond do
+      is_list(desc) -> to_string(desc)
+      true -> inspect(desc)
+    end
+  end
 
 end
