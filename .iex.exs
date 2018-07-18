a12260752bf344557382036adc4d17ef3dd37e46
Sheharyar Naseer <hello@sheharyar.me>
2018-07-18 12:44:49 -0400

    More IEx aliases/helpers

--- .iex.exs
+++ .iex.exs
@@ -1,9 +1,17 @@
 alias Memento.{
   Table,
   Query,
+  Schema,
   Mnesia,
+
+  Error,
+  MnesiaException,
 
   Support,
   Support.Definitions,
 }
+
+
+# User Table
+table = Definitions.Table.User
 
