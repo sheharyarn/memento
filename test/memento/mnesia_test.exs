5f113cb574ceb3cbdecda8fc81b6612c8e0b4d46
Sheharyar Naseer <hello@sheharyar.me>
2018-07-18 12:40:29 -0400

    Testing errors raised by Mnesia.call

--- test/memento/mnesia_test.exs
+++ test/memento/mnesia_test.exs
@@ -2,16 +2,40 @@ defmodule Memento.Tests.Mnesia do
   use Memento.Support.Case
 
   alias Memento.Mnesia
+  alias Memento.MnesiaException
   require Mnesia
 
 
   describe "#call" do
+    setup do
+      Support.Mnesia.stop
+      :ok
+    end
+
+
     @func :system_info
     @args [:is_running]
-
     test "delegates method calls to the mnesia module" do
-      assert :yes == Mnesia.call(@func, @args)
+      assert :no == Mnesia.call(@func, @args)
     end
-  end
 
+
+    @func :schema
+    @args []
+    test "re-raises mnesia exits as memento exceptions" do
+      assert_raise(MnesiaException, ~r/not running/i, fn ->
+        Mnesia.call(@func, @args)
+      end)
+    end
+
+
+    @func :table_info
+    @args [Tables.User, :all]
+    test "prints descriptions of the error" do
+      assert_raise(MnesiaException, ~r/tried to perform op on non-existing/i, fn ->
+        Mnesia.call(@func, @args)
+      end)
+    end
+
+  end
 end
