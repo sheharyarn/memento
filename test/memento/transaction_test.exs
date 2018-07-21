4106e2bef2a6423b868f004dd29f1a3dd1eea913
Sheharyar Naseer <hello@sheharyar.me>
2018-07-20 22:10:29 -0400

    Tests for Transaction.execute

---
+++ test/memento/transaction_test.exs
@@ -0,0 +1,48 @@
+defmodule Memento.Tests.Transaction do
+  use Memento.Support.Case
+  alias Memento.Transaction
+
+
+  # Don't need to extensively rest these methods, just how
+  # they return back the data.
+
+
+  describe "#execute" do
+    test "re-raises mnesia rescues as original errors" do
+      assert_raise(UndefinedFunctionError, ~r/is undefined/i, fn ->
+        Transaction.execute fn ->
+          RandomModule.undefined_fun
+        end
+      end)
+    end
+
+
+    test "function is actually executed inside a transaction" do
+      Transaction.execute fn ->
+        assert :mnesia.is_transaction
+      end
+    end
+  end
+
+
+
+
+  describe "#execute!" do
+    test "re-raises mnesia rescues as original errors" do
+      assert_raise(UndefinedFunctionError, ~r/is undefined/i, fn ->
+        Transaction.execute! fn ->
+          RandomModule.undefined_fun
+        end
+      end)
+    end
+
+
+    test "function is actually executed inside a transaction" do
+      Transaction.execute! fn ->
+        assert :mnesia.is_transaction
+      end
+    end
+  end
+
+
+end
