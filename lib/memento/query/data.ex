48c17c42ed548c98448d0be241fc715ebb4cf26a
Sheharyar Naseer <hello@sheharyar.me>
2018-07-22 17:45:00 -0400

    Rename Data.cast to Data.load, and add more docs

--- lib/memento/query/data.ex
+++ lib/memento/query/data.ex
@@ -1,15 +1,32 @@
 defmodule Memento.Query.Data do
   @moduledoc """
-  This module acts as an interface to convert Memento Table data
-  structs into Mnesia data tuples and vice versa.
+  Helper module that acts as an interface to convert Memento Table
+  data structs into Mnesia data tuples and vice versa.
+
+  This is responsible for automatically handling conversions
+  between data in methods defined in the Query module, so you
+  won't need to use this at all.
   """
+
+
+
+  # Public API
+  # ----------
 
 
   @doc """
-  Automatically cast Mnesia data into Memento Table struct.
+  Convert Mnesia data tuple into Memento Table struct.
+
+  Use this method when reading data from an Mnesia database. The
+  data should be in a tuple format, where the first element is
+  the Table name (`Memento.Table` definition).
+
+  This will automatically match the the tuple values against the
+  table's attributes and convert it into a struct of the Memento
+  table you defined.
   """
-  @spec cast(tuple) :: Memento.Table.data
-  def cast(data) when is_tuple(data) do
+  @spec load(tuple) :: Memento.Table.data
+  def load(data) when is_tuple(data) do
     [table | values] =
       Tuple.to_list(data)
 
@@ -20,5 +37,6 @@ defmodule Memento.Query.Data do
 
     struct(table, values)
   end
+
 
 end
