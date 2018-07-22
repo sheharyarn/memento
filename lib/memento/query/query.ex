49f1cd895c6e8e186a27173b99e3f2813ca7e734
Sheharyar Naseer <hello@sheharyar.me>
2018-07-22 18:40:45 -0400

    Implemented Query.read

--- lib/memento/query/query.ex
+++ lib/memento/query/query.ex
@@ -1,6 +1,15 @@
 defmodule Memento.Query do
+  require Memento.Mnesia
+
+  alias Memento.Query
+  alias Memento.Table
+  alias Memento.Mnesia
+
+
   @moduledoc """
-  Module to read/write from Memento Tables.
+  Module to read/write from Memento Tables. All of the methods in
+  this module need to be executed within the context of a Memento
+  Transaction.
   """
 
 
@@ -8,7 +17,27 @@ defmodule Memento.Query do
   # ----------
 
 
-  def read
+  @doc """
+  Finds the Memento record for the given id in the specified table.
+
+  If no record is found, `nil` is returned. You can also pass an
+  optional 3rd argument `lock`, which acquires a lock of that type.
+  Defaults to `:read`. See `t:lock` for more details.
+
+  This method works a bit differently from the original `:mnesia.read/3`
+  when the table type is `:bag`. Since a bag can have many records
+  with the same key, this returns only the first one. If you want to
+  fetch all records with the given key, use `match/2` or `select/2`.
+  """
+  @spec read(Table.t, any, lock) :: Table.data | nil
+  def read(table, id, lock \\ :read) do
+    case Mnesia.call(:read, [table, id, lock]) do
+      []           -> nil
+      [record | _] -> Query.Data.load(record)
+    end
+  end
+
+
   def write
   def all
   def first
