97ab8fc93a2dadd47a2c9810cd5fa4da441d6209
Sheharyar Naseer <hello@sheharyar.me>
2018-07-22 17:21:13 -0400

    New Query.Data module for casting

---
+++ lib/memento/query/data.ex
@@ -0,0 +1,22 @@
+defmodule Memento.Query.Data do
+  @moduledoc """
+  This module acts as an interface to convert Memento Table data
+  structs into Mnesia data tuples and vice versa.
+  """
+
+
+  @doc "Cast Mnesia data into Memento Table struct"
+  @spec cast(tuple) :: Memento.Table.data
+  def cast(data) when is_tuple(data) do
+    [table | values] =
+      Tuple.to_list(data)
+
+    values =
+      table.__info__()
+      |> Map.get(:attributes)
+      |> Enum.zip(values)
+
+    struct(table, values)
+  end
+
+end
