502a5e69f098ecec7e1291898483f82b53bbc338
Sheharyar Naseer <hello@sheharyar.me>
2018-08-29 14:49:12 -0400

    Bump version to v0.2.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Memento.Mixfile do
 
   @app     :memento
   @name    "Memento"
-  @version "0.2.0"
+  @version "0.2.1"
   @github  "https://github.com/sheharyarn/#{@app}"
   @author  "Sheharyar Naseer"
   @license "MIT"
