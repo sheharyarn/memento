9bf996e8d8c5346e97d83c8062875ddcd4fc5cd6
Hew <github@mhs.fastmail.com>
2019-12-03 15:01:30 -0500

    Get rid of a case statement

--- lib/memento/strategy/ram/startup_ram.ex
+++ lib/memento/strategy/ram/startup_ram.ex
@@ -7,20 +7,15 @@ defmodule Memento.Strategy.Startup.RAM do
   @impl true
   def execute(tables, nodes \\ Node.list()) do
     {:ok, _} = Memento.Mnesia.call(:change_config, [:extra_db_nodes, nodes])
-
-    Enum.each(tables, fn x ->
-      case Memento.Table.create(x) do
-        :ok ->
-          :ok
-
-        {:error, {:already_exists, _}} ->
-          Memento.Mnesia.call(:add_table_copy, [x, node(), :ram_copies])
-
-        _ ->
-          throw(x)
-      end
-    end)
-
-    Memento.Mnesia.call(:wait_for_tables, [tables, @wait_timeout])
+    Enum.each(tables, fn x -> Memento.Table.create(x) |> handle_result(x) end)
+    :ok = Memento.Mnesia.call(:wait_for_tables, [tables, @wait_timeout])
   end
+
+  defp handle_result(:ok, _table), do: :ok
+
+  defp handle_result({:error, {:already_exists, _}}, table) do
+    Memento.Mnesia.call(:add_table_copy, [table, node(), :ram_copies])
+  end
+
+  defp handle_result(error, _), do: throw(error)
 end
